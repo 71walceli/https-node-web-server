5ccfc194e94ed4129af54b206ac4e530816ed4b8
Walter Celi <antonini2903@hotmail.com>
2022-12-08 22:50:13 -0500

    Modified services JSON structure

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ import "dotenv/config"
 import { init } from 'vhttps';
 import fs from "fs"
 import httpProxyMiddleware from 'http-proxy-middleware'
-import httpProxy from 'http-proxy'
 import express from "express";
 import cors from "cors"
+import bodyParser from "body-parser"
 
 const vhttpsServer = init();
-
 vhttpsServer.setOptions({
     cert: fs.readFileSync(process.env.DEFAULT_SSL_CERT),
     key: fs.readFileSync(process.env.DEFAULT_SSL_KEY),
@@ -16,81 +15,86 @@ vhttpsServer.setOptions({
 
 const registerProxiedService = (endpoint) => {
     const expressServer = express()
-    let internalProxyServer = {}
-    let internalProxyOptions = {}
-    const proxyOptions = {
-        changeOrigin: true,
-        logLevel: "debug",
-        pathRewrite: (path) => endpoint.globalEndpoint && `${path.replace(endpoint.globalEndpoint.replace("*", ""), "")
-            .replace(/\/{2,}/ig, "/")}`
-            || endpoint.globalEndpoint === "/" && path
-            || path,
-        onProxyRes: (proxyRes, req, res) => {
-            if (endpoint?.proxyOptions?.tryPreventRedirectionLoops
-                && [301,302,303,304,305,306,307].includes(proxyRes.statusCode) 
-                && proxyRes?.headers?.location
-            ) {
-                proxyRes.url = proxyRes.headers.location
-                req.url = `${req.protocol}://${req.hostname}${req.originalUrl || "/"}`;
-                if (req.url === proxyRes.headers.location) {
-                    proxyRes.statusCode = 200
-                    delete proxyRes.headers.location;
-                    internalProxyServer.web(req, res, internalProxyOptions, next)
-                    console.log({
-                        requestedUrl: req.url,
-                        redirectedUrl: proxyRes.url,
-                        message: "Redirect prevented",
-                    })
-                }
-                else
-                    console.log({
-                        requestedUrl: req.url,
-                        redirectedUrl: proxyRes.url,
-                        message: "Redirect passed",
-                    })
+
+    if (endpoint.urls) {
+        for (let url in endpoint.urls) {
+            const router = express.Router()
+            const urlConfig = endpoint.urls[url]
+            const proxyOptions = {
+                changeOrigin: true,
+                logLevel: "debug",
+                pathRewrite: (path) => 
+                    url && `${path.replace(url
+                        .replace("*", ""), "")
+                        .replace(/\/{2,}/ig, "/")}`
+                    || url === "/" && path
+                    || path,
+                // TODO Review timeout
+                //proxyTimeout: 5000,
+                target: urlConfig.destination,
+                ...urlConfig.proxyOptions
+            };
+            const middleware = httpProxyMiddleware.createProxyMiddleware(proxyOptions);
+            
+            // TODO Set this if no global config given
+            if (urlConfig.cors) {
+                router.use(cors())
             }
-        },
-        proxyTimeout: 5000,
-        target: endpoint.localEndpoint,
-        ...endpoint.proxyOptions
-    };
-    const middleware = httpProxyMiddleware.createProxyMiddleware(proxyOptions);
-    
-    internalProxyOptions = {
-        changeOrigin: true,
-        followRedirects: true,
-        logLevel: "debug",
-        proxyTimeout: 5000,
-        target: endpoint.localEndpoint,
-        ...endpoint.proxyOptions
-    };
-    if (internalProxyOptions.hostRewrite)
-        internalProxyOptions.hostRewrite = "http"
-    if (internalProxyOptions.protocolRewrite)
-        internalProxyOptions.protocolRewrite = endpoint.localEndpoint
-    internalProxyServer = httpProxy.createProxyServer(internalProxyOptions)
-    
-    if (endpoint.cors) {
-        expressServer.use(cors())
-    }
-    if (endpoint.globalEndpoint) {
-        expressServer.use(endpoint.globalEndpoint, middleware)
-        expressServer.use("*", (req, res) => res.status(404).end("Not Found"))
+            router.use(middleware)
+            // TODO call next
+            // TODO Add logging
+            //  - Local and destination IPs
+            //  - Local and destination endpoints
+            //  - Timestamp
+            //  - HTTP status code
+            //  - Headers
+            //  - Other connection errors
+            if (urlConfig.rewriteHeaders) {
+                middleware.onProxyReq((proxyReq, res, req) => {
+                    for (const header in urlConfig.headersRewrite) {
+                        proxyReq.headers[header] = urlConfig.headersRewrite[header]
+                    }
+                })
+            }
+            if (urlConfig.keepAliveOptions?.timeout) {
+                router.keepAliveTimeout = urlConfig.keepAliveOptions.timeout;
+                router.headersTimeout = urlConfig.keepAliveOptions.timeout;
+                middleware.keepAliveTimeout = urlConfig.keepAliveOptions.timeout;
+                middleware.headersTimeout = urlConfig.keepAliveOptions.timeout;
+            }
+            expressServer.use(url, router)
+            //expressServer.use((req, res) => res.status(404).end("Not Found"))
+        }
     } else {
+        const proxyOptions = {
+            changeOrigin: true,
+            logLevel: "debug",
+            /*
+            pathRewrite: (path) => endpoint.globalEndpoint && `${path.replace(endpoint.globalEndpoint.replace("*", ""), "")
+                .replace(/\/{2,}/ig, "/")}`
+                || endpoint.globalEndpoint === "/" && path
+                || path,
+            proxyTimeout: 5000,
+            */
+            target: endpoint.destination,
+            ...endpoint.proxyOptions
+        };
+        const middleware = httpProxyMiddleware.createProxyMiddleware(proxyOptions);
         expressServer.use(middleware)
-    }
-    if (endpoint.rewriteHeaders) {
-        middleware.onProxyReq((proxyReq, res, req) => {
-            for (const header in endpoint.headersRewrite) {
-                proxyReq.headers[header] = endpoint.headersRewrite[header]
-            }
-        })
-    }
-    if (endpoint?.keepAliveOptions?.timeout) {
-        expressServer.keepAliveTimeout = endpoint.keepAliveOptions.timeout;
-        expressServer.headersTimeout = endpoint.keepAliveOptions.timeout;
-        middleware.keepAliveTimeout = endpoint.keepAliveOptions.timeout;
-        middleware.headersTimeout = endpoint.keepAliveOptions.timeout;
+        
+        if (endpoint.rewriteHeaders) {
+            middleware.onProxyReq((proxyReq, res, req) => {
+                for (const header in endpoint.headersRewrite) {
+                    proxyReq.headers[header] = endpoint.headersRewrite[header]
+                }
+            })
+        }
+        if (endpoint?.keepAliveOptions?.timeout) {
+            expressServer.keepAliveTimeout = endpoint.keepAliveOptions.timeout;
+            expressServer.headersTimeout = endpoint.keepAliveOptions.timeout;
+            middleware.keepAliveTimeout = endpoint.keepAliveOptions.timeout;
+            middleware.headersTimeout = endpoint.keepAliveOptions.timeout;
+        }
     }
     vhttpsServer.use(endpoint.publicDomain
         ,{
@@ -104,6 +108,7 @@ const registerProxiedService = (endpoint) => {
         ,expressServer
     );
     // TODO log every request
+
 }
 
 
@@ -122,4 +127,12 @@ vhttpsServer.use((req, res) => {
 });
 
 vhttpsServer.listen(443);
- 
+
+
+const internalServer = express()
+internalServer.use(bodyParser.json());
+internalServer.post("/__register", (req, res, next) => {
+    // TODO WIP as this won't actually work!
+    registerProxiedService(req.body)
+})
+internalServer.listen(80)
